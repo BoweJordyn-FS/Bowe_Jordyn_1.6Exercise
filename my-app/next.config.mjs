5ec9d814673011ba1197c601a25267498b262ee3
BoweJordyn-FS <jkbowe@student.fullsail.edu>
2026-06-07 13:06:37 -0400

    Deploy from branch => Github Actions

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/Bowe_Jordyn_1.6Exercise',
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
