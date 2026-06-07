ff17ace9790702575da60cd55f05cca653fbd60b
BoweJordyn-FS <jkbowe@student.fullsail.edu>
2026-06-07 13:30:54 -0400

    image fix

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/Bowe_Jordyn_1.6Exercise',
-  images: { unoptimized: true },
+	output: 'export',
+	basePath: '/Bowe_Jordyn_1.6Exercise',
+	images: { unoptimized: true },
 };
 
 export default nextConfig;
